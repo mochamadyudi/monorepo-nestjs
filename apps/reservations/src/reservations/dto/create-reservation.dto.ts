837754cb5a6e4cf9af2fe485dd00f6990c61f11a
mochamadyudi <myudisobari12@gmail.com>
2023-07-07 01:38:55 +0700

    create validator

--- apps/reservations/src/reservations/dto/create-reservation.dto.ts
+++ apps/reservations/src/reservations/dto/create-reservation.dto.ts
@@ -1,8 +1,19 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 export class CreateReservationDto {
-  timestamp: Date;
-  startData: Date;
+  @IsDate()
+  @Type(() => Date)
+  startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
-  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
